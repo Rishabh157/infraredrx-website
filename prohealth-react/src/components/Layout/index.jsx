b0b7117ae65d99d1c805a64698d077d171f88b5b
Rishabh157 <rishabhgour157@gmail.com>
2025-02-13 10:38:56 +0530

    initial commit with offical logo

---
+++ prohealth-react/src/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Footer';
+import Header from '../Header';
+
+export default function Layout() {
+  return (
+    <>
+      {/* <Header logoSrc="/images/logo.svg" variant="cs_heading_color" /> */}
+      <Header logoSrc="/INFRAREDRX.png" variant="cs_heading_color" />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
